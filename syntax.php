d835eacf79a09adbb2a45f28fc8d3d54b6247dbe
Myron Turner <turnermm02@shaw.ca>
2018-09-15 06:20:03 -0500

    Adds optional classes to be specified for the css_per_page div

--- syntax.php
+++ syntax.php
@@ -30,13 +30,22 @@ class syntax_plugin_cssperpage extends DokuWiki_Syntax_Plugin {
 
 
     function connectTo($mode) {    
-     $this->Lexer->addSpecialPattern('~~cssp(.*)openDIV~~',$mode,'plugin_cssperpage');
+     $this->Lexer->addSpecialPattern('~~cssp_openDIV~~',$mode,'plugin_cssperpage');
      $this->Lexer->addSpecialPattern('~~cssp_closeDIV~~',$mode,'plugin_cssperpage');
+      $this->Lexer->addSpecialPattern('~~cssp_\w+_openDIV~~',$mode,'plugin_cssperpage');
+     
     }
 
 
-    function handle($match, $state, $pos, &$handler){
-       $match = substr($match,7,-2);       
+    function handle($match, $state, $pos,  Doku_Handler $handler){
+      
+    //    if(strlen($match) > 16) {
+          
+           if(preg_match("/~~cssp_(.*?)_openDIV/",$match,$matches)) {  
+            $class=$matches[1];
+            $match = $class . ';' . 'openDIV';
+        }
+       else $match = substr($match,7,-2);       
         switch ($state) {   
           case DOKU_LEXER_SPECIAL :
            return array($state, $match);
@@ -45,17 +54,26 @@ class syntax_plugin_cssperpage extends DokuWiki_Syntax_Plugin {
     }
 
 
-    function render($mode, &$renderer, $data) {
+    function render($mode,  Doku_Renderer $renderer, $data) {
         if($mode == 'xhtml'){
            list($state,$match) = $data;
             switch ($state) {          
-              case DOKU_LEXER_SPECIAL :    
-                if($match == 'openDIV') {
-                   $renderer->doc .= "\n<div id='css_per_page'  class = 'cssp_1 cssp_2  cssp_3'>\n";                    
+              case DOKU_LEXER_SPECIAL :
+             
+               list($class,$rest) = explode(';',$match);
+                if($rest) {                    
+                    $match = 'openDIV';                  
+                }
+                 else {
+                     $class ='opencss';                  
+                }       
+                if($match == 'openDIV') {                  
+                  $renderer->doc .= "\n<div id='css_per_page'  class = '" . $class ."'>\n";                    
                 }                
                 elseif($match == 'closeDIV') {
                    $renderer->doc .= "\n</div>\n";                    
-                }                                
+                }  
+                               
                return true;
             }
         }
